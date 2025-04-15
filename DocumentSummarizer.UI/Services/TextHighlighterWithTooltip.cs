e5fe2a2ad00b984af261cf725908d8c592818dc5
Mahantesh GP <mahanteshpujar1009@gmail.com>
2025-04-16 00:09:02 +0530

    added

---
+++ DocumentSummarizer.UI/Services/TextHighlighterWithTooltip.cs
@@ -0,0 +1,29 @@
+
+using System.Text.RegularExpressions;
+
+namespace YourNamespace.Helpers
+{
+    public static class TextHighlighter
+    {
+        public static string HighlightTerms(string text, string query)
+        {
+            if (string.IsNullOrWhiteSpace(text) || string.IsNullOrWhiteSpace(query))
+                return text;
+
+            var terms = query.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            foreach (var term in terms.Distinct(StringComparer.OrdinalIgnoreCase))
+            {
+                var pattern = Regex.Escape(term);
+                text = Regex.Replace(
+                    text,
+                    pattern,
+                    match => $"<span title='Matched term' style='background-color: yellow; font-weight: bold'>{match.Value}</span>",
+                    RegexOptions.IgnoreCase | RegexOptions.CultureInvariant
+                );
+            }
+
+            return text;
+        }
+    }
+}
